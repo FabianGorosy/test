7ac194b395d73c36678e187c76e34cc024a859ec
Fabian Gorosky <fabiangorosky@gmail.com>
2020-10-21 15:43:33 -0300

    cambio para remoto

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,6 @@
 @section('content')
     <div class="container">
         <h1>Hola Livewire probando git</h1>
-        <p>Lorem, ipsum dolor sit amet consecteturwwwwwww adipisicing elit. Aliquid exercitationem, ad libero magnam odit autem inventore, alias necessitatibus, iste labore facilis illo vitae quis. 
-            Accusamus voluptatum eaque maxime rerum blanditiis?estamos en prueba</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid exercitationem, ad libero magnam odit autem inventore, alias necessitatibus, iste labore facilis illo vitae quis. Accusamus voluptatum eaque maxime rerum blanditiis?</p>
     </div>
 @endsection
