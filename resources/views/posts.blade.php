2654f7dc4b07d04891ea3f9dbde2159728f7547d
Fabian Gorosky <fabiangorosky@gmail.com>
2020-10-21 15:26:05 -0300

    cambio 1

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,6 +3,6 @@
     <div class="container">
         <h1>Hola Livewire probando git</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid exercitationem, ad libero magnam odit autem inventore, alias necessitatibus, iste labore facilis illo vitae quis. 
-            Accusamus voluptatum eaque maxime rerum blanditiis?</p>
+            Accusamus voluptatum eaque maxime rerum blanditiis?esta mos en prueba</p>
     </div>
 @endsection
